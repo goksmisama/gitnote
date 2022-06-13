ba887b1e74f6a05a59479cee183dc312250feb5b
marui <984603294@qq.com>
2022-06-13 22:40:14 +0800

    add a folder

--- 更新保存.bat
+++ 更新保存.bat
@@ -1,7 +1,7 @@
 @echo off
-git pull origin master:brantest
+git pull origin main:brantest
 git add -A
 set /p commit_log= Please write commit message:
 git commit -m "%commit_log%"
-git push origin master
+git push origin main
 pause;
